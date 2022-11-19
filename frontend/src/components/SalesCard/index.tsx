33c78b82e74e84dae80bd855393d29d693d390c9
Lucas França <lucasfreitas_c5@hotmail.com>
2022-11-19 14:19:43 -0300

    conectando front com back

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import NotificationButton from '../NotificationButton'
-import './styles.css'
+import NotificationButton from '../NotificationButton';
+import './styles.css';
 
 function SalesCard() {
 
@@ -11,6 +12,11 @@ function SalesCard() {
     const [minDate, setMinDate] = useState(min);
     const [maxDate, setMaxDate] = useState(new Date());
 
+    useEffect(() => {
+        axios.get("http://localhost:8080/sales").then(response => {
+            console.log(response.data);
+        });
+    }, []);
 
     return (
         <div className="dsmeta-card">
@@ -96,4 +102,4 @@ function SalesCard() {
     )
 }
 
-export default SalesCard
+export default SalesCard;
